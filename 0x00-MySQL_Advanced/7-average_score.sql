3b91d2fda6cd34dca691fddd331e047844da238d
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-21 21:02:12 +0000

    MySQL

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN p_user_id INT
+)
+BEGIN
+    DECLARE avg_score DECIMAL(10, 2);
+
+    -- Compute the average score for the specified user
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = p_user_id;
+
+    -- Update the average score in the users table
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = p_user_id;
+END //
+
+DELIMITER ;
